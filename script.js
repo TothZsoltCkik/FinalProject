b9f238bb1cd5cd93d4e327864cf764fe9cc7da41
TothZsoltCkik <toth.zsolt@ckik.com>
2026-03-22 20:17:34 +0100

    SearchBar

--- script.js
+++ script.js
@@ -1,39 +1,66 @@
 const container = document.getElementById("mealsContainer");
 
 async function loadRandom() {
-  container.innerHTML = "Loading...";
+    container.innerHTML = "Loading...";
 
-  const requests = [];
+    const requests = [];
 
-  for (let i = 0; i < 14; i++) {
-      requests.push(fetch("https://www.themealdb.com/api/json/v1/1/random.php"));
-  }
+    for (let i = 0; i < 14; i++) {
+        requests.push(fetch("https://www.themealdb.com/api/json/v1/1/random.php"));
+    }
 
-  const responses = await Promise.all(requests);
+    const responses = await Promise.all(requests);
 
-  const results = [];
-  for (const res of responses) {
-      const data = await res.json();
-      results.push(data);
-  }
+    const results = [];
+    for (const res of responses) {
+        const data = await res.json();
+        results.push(data);
+    }
 
-  container.innerHTML = "";
+    container.innerHTML = "";
 
-  for (const res of results) {
-      addMealCard(res.meals[0]);
-  }
+    for (const res of results) {
+        addMealCard(res.meals[0]);
+    }
 }
 
 function addMealCard(meal) {
-  const div = document.createElement("div");
-  div.classList.add("card");
+    const div = document.createElement("div");
+    div.classList.add("card");
 
-  div.innerHTML = `
-      <img src="${meal.strMealThumb}"><h3>${meal.strMeal}</h3>`;
+    div.innerHTML = `<img src="${meal.strMealThumb}"><h3>${meal.strMeal}</h3>`;
 
-  div.onclick = () => showDetails(meal);
+    div.onclick = () => showDetails(meal);
 
-  container.appendChild(div);
+    container.appendChild(div);
+}
+
+
+async function searchMeals() {
+    const query = document.getElementById("searchInput").value;
+
+    if (!query) return;
+
+    container.innerHTML = "Search...";
+
+    const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+    const data = await res.json();
+
+    displayMeals(data.meals);
+}
+
+
+function displayMeals(meals) {
+    container.innerHTML = "";
+
+    if (!meals) {
+        container.innerHTML = "<p>Not found</p>";
+        return;
+    }
+
+    for (const element of meals) {
+        addMealCard(element)
+    }
 }
 
 loadRandom();
